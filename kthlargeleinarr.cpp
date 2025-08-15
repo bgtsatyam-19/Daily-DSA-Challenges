1f31b5c292e830fccd69095bd84a79b025fd3156
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-08-15 21:41:51 +0530

    Add files via upload

---
+++ kthlargeleinarr.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+         priority_queue<int, vector<int>, greater<int>> minHeap;
+        
+        for (int num : nums) {
+            minHeap.push(num); 
+            if (minHeap.size() > k) { 
+                minHeap.pop(); 
+            }
+        }
+        return minHeap.top();
+    }
+};
+int main(){
+    Solution obj;
+    vector<int> nums = {3,2,1,5,6,4};
+    cout<<obj.findKthLargest(nums,2)<<endl; 
+}
