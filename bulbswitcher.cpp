8c794690be10ee2846f064aa2e541598a7b9e0d8
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-08-30 16:11:39 +0530

    Add files via upload

---
+++ bulbswitcher.cpp
@@ -0,0 +1,13 @@
+//bulb switcher
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+int main(){
+    Solution s;
+    cout<<s.bulbSwitch(3)<<endl;
+}
