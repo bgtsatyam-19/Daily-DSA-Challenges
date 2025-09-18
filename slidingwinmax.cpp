b1a68649275c03f3b37520b2c5cab6f1f8395010
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-09-18 22:52:52 +0530

    Add files via upload

---
+++ slidingwinmax.cpp
@@ -0,0 +1,33 @@
+//Sliding Window Maximum
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+        return result;
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3;
+    vector<int> result = s.maxSlidingWindow(nums, k);
+    for (int num : result) {
+        cout << num << " ";
+    }
+    return 0;
+}
