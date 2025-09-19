d0cd2c32af0c038b6ef080ac9854ea65a7b141fe
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-09-19 21:41:30 +0530

    Add files via upload

---
+++ mediandata.cpp
@@ -0,0 +1,42 @@
+//Find Median from Data Stream 
+#include<bits/stdc++.h>
+using namespace std;
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap; 
+    priority_queue<int, vector<int>, greater<int>> minHeap; // Upper half
+
+    MedianFinder() {}
+
+    void addNum(int num) {
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double findMedian() {
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.top();
+        } else {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        }
+    }
+};
+int main(){
+    MedianFinder mf;
+    mf.addNum(1);
+    mf.addNum(2);
+    cout << mf.findMedian() << endl; 
+    mf.addNum(3);
+    cout << mf.findMedian() << endl; 
+    return 0;
+}
