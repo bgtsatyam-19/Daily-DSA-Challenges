0f254b57499b67bd50cea211bc835110ea7dbf0e
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-10-07 17:54:54 +0530

    Add files via upload

---
+++ isubseq.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) i++;
+            j++;
+        }
+        return i == s.size();
+    }
+};
+
+int main() {
+    Solution obj;
+    string s = "abc", t = "ahbgdc";
+    cout << boolalpha << obj.isSubsequence(s, t);
+    return 0;
+}
