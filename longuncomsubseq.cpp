ee75d0de9bcc2558fd706db1481b63e566be3b92
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-10-15 20:16:37 +0530

    Add files via upload

---
+++ longuncomsubseq.cpp
@@ -0,0 +1,16 @@
+//Longest Uncommon Subsequence I
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int findLUSlength(string a, string b) {
+        if(a==b) return -1;
+        return max(a.length(),b.length());
+    }
+};
+int main(){
+    Solution obj;
+    string a="aba",b="cdc";
+    cout<<obj.findLUSlength(a,b)<<endl;
+    return 0;
+}
