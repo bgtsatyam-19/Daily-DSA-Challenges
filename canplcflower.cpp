d99cb76e3c8a0de50ba5778ccb25df399151e16c
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-10-21 22:02:53 +0530

    Add files via upload

---
+++ canplcflower.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        int size = flowerbed.size();
+        for(int i = 0; i < size; i++) {
+            if(flowerbed[i] == 0) {
+                bool emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                bool emptyRight = (i == size - 1) || (flowerbed[i + 1] == 0);
+                if(emptyLeft && emptyRight) {
+                    flowerbed[i] = 1;
+                    count++;
+                    if(count >= n) return true;
+                }
+            }
+        }
+        return count >= n;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> flowerbed = {1, 0, 0, 0, 1};
+    int n = 1;
+    cout << boolalpha << obj.canPlaceFlowers(flowerbed, n) << endl;
+    return 0;
+}
