88fe6ee4f0d14e3516cbeb9107fce5e4250ebe87
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-11-11 18:32:16 +0530

    Add files via upload

---
+++ longestrepchrrep.cpp
@@ -0,0 +1,29 @@
+//Longest Repeating Character Replacement
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    int characterReplacement(string s, int k) {
+        vector<int> count(26,0);
+        int maxCount=0;
+        int left=0;
+        int result=0;
+        for(int right=0;right<s.size();right++){
+            count[s[right]-'A']++;
+            maxCount=max(maxCount,count[s[right]-'A']);
+            while((right-left+1)-maxCount>k){
+                count[s[left]-'A']--;
+                left++;
+            }
+            result=max(result,right-left+1);
+        }
+        return result;
+    }
+};
+int main(){
+    Solution obj;
+    string s="AABABBA";
+    int k=1;
+    cout<<obj.characterReplacement(s,k)<<endl; 
+    return 0;
+}
