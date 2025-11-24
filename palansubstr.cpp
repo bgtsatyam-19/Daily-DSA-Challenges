88e3b1e117f3dc4d21bccf7d6d31c518064d7426
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-11-24 22:57:55 +0530

    Add files via upload

---
+++ palansubstr.cpp
@@ -0,0 +1,26 @@
+//Palindromic Substrings
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (s[i] == s[j] && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+};
+int main(){
+    Solution obj;
+    string s = "abc";
+    cout << obj.countSubstrings(s) << endl; 
+    return 0;
+}
