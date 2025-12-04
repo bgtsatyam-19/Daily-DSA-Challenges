dfbc3690e0e8891149b97ed01e89cfb2ecd8c862
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2025-12-04 22:05:12 +0530

    Add files via upload

---
+++ palindpart.cpp
@@ -0,0 +1,38 @@
+//Palindrome Partitioning II
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minCut(string s) {
+        int n = s.size();
+        vector<vector<bool>> isPalindrome(n, vector<bool>(n, false));
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (s[i] == s[j] && (j - i < 2 || isPalindrome[i + 1][j - 1])) {
+                    isPalindrome[i][j] = true;
+                }
+            }
+        }
+
+        vector<int> dp(n, INT_MAX);
+        for (int i = 0; i < n; ++i) {
+            if (isPalindrome[0][i]) {
+                dp[i] = 0;
+            } else {
+                for (int j = 0; j < i; ++j) {
+                    if (isPalindrome[j + 1][i]) {
+                        dp[i] = min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+};
+
+int main(){
+    Solution obj;
+    string s = "aab";
+    cout << obj.minCut(s) << endl; 
+    return 0;
+}
