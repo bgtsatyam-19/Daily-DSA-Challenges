dbb451adb1900b81dca463407e11072330ce7844
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2026-01-23 20:46:32 +0530

    Add files via upload

---
+++ permutations2.cpp
@@ -0,0 +1,50 @@
+//permutations 2
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end());
+        backtrack(nums, used, current, result);
+        return result;
+    }
+private:
+    void backtrack(const vector<int>& nums, vector<bool>& used, vector<int>& current
+        , vector<vector<int>>& result) {
+        if (current.size() == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue; 
+            used[i] = true;
+            current.push_back(nums[i]);
+            backtrack(nums, used, current, result);
+            current.pop_back();
+            used[i] = false;
+        }
+    }
+};
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+    vector<vector<int>> result = obj.permuteUnique(nums);
+
+    for (const auto& perm : result) {
+        for (int num : perm) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
