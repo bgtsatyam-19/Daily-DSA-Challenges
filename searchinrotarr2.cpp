fa563106de8771c32c566fdc79ca4f4a3bac00b6
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2026-01-27 22:48:25 +0530

    Add files via upload

---
+++ searchinrotarr2.cpp
@@ -0,0 +1,46 @@
+//search in rotated sorted array II
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return true;
+
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                ++left;
+                --right;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
+   int main() {
+    int n, target;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    cin >> target;
+
+    Solution obj;
+    bool found = obj.search(nums, target);
+    cout << (found ? "Found" : "Not Found") << endl;
+
+    return 0;
+}
