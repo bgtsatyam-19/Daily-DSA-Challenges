483e4b7d93600334cc444c87c55eb389b216c665
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2026-01-29 21:46:54 +0530

    Add files via upload

---
+++ oddeenli.cpp
@@ -0,0 +1,58 @@
+///Odd Even Linked List
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+
+        return head;
+    }
+};
+int main() {
+    int n;
+    cin >> n;
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+
+    for (int i = 0; i < n; ++i) {
+        int val;
+        cin >> val;
+        ListNode* newNode = new ListNode(val);
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = tail->next;
+        }
+    }
+
+    Solution sol;
+    ListNode* result = sol.oddEvenList(head);
+
+    while (result) {
+        cout << result->val << " ";
+        result = result->next;
+    }
+    cout << endl;
+
+    return 0;
+}
