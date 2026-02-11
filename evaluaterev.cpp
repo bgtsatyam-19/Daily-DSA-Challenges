49fad60134a6732fbb6d3b04a82a864ebaabd655
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2026-02-11 21:40:39 +0530

    Add files via upload

---
+++ evaluaterev.cpp
@@ -0,0 +1,44 @@
+//Evaluate Reverse Polish Notation
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    int evalRPN(vector<string> &tokens)
+    {
+        stack<int> st;
+        for (const string &token : tokens)
+        {
+            if (token == "+" || token == "-" || token == "*" || token == "/")
+            {
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+                if (token == "+")
+                    st.push(a + b);
+                else if (token == "-")
+                    st.push(a - b);
+                else if (token == "*")
+                    st.push(a * b);
+                else
+                    st.push(a / b);
+            }
+            else
+            {
+                st.push(stoi(token));
+            }
+        }
+        return st.top();
+    }
+};
+int main()
+{
+    Solution solution;
+    vector<string> tokens = {"2", "1", "+", "3", "*"};
+    cout << solution.evalRPN(tokens) << endl; // Output: 9
+    return 0;
+}
