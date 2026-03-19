e6f29229fec65b63df93565ec1d5d4b70b3e3892
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2026-03-19 20:40:33 +0530

    Add files via upload

---
+++ firstbadvs.cpp
@@ -0,0 +1,26 @@
+//First Bad Version
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isBadVersion(int version) {
+    return version >= 4; 
+}
+
+int firstBadVersion(int n) {
+    int left = 1;
+    int right = n;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (isBadVersion(mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+int main() {
+    int n = 5; // Example input
+    cout << "First bad version: " << firstBadVersion(n) << endl;
+    return 0;
+}
