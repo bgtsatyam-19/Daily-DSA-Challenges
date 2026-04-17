66700ae69258b20e744ac6a59076d4a20957657c
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2026-04-17 23:09:32 +0530

    Add files via upload

---
+++ btlevelordertrans.cpp
@@ -0,0 +1,63 @@
+//Binary Tree Level Order Traversal
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) return result;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> currentLevel;
+            
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                currentLevel.push_back(node->val);
+                
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            result.push_back(currentLevel);
+        }
+        
+        return result;
+    }
+};
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    
+    Solution solution;
+    vector<vector<int>> result = solution.levelOrder(root);
+    
+    cout << "Level order traversal of the binary tree:" << endl;
+    for (const auto& level : result) {
+        for (int val : level) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    
+    // Clean up memory
+    delete root->left->left;
+    delete root->left->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+    
+    return 0;
+}
