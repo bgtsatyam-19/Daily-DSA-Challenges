bb780d88a2b156b59b1e242511680bf25ff7ee3d
Satyam Bhagat <163175345+bgtsatyam-19@users.noreply.github.com>
2026-05-05 23:32:52 +0530

    Add files via upload

---
+++ searcha2dmat.cpp
@@ -0,0 +1,48 @@
+//Search a 2D Matrix II
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return false;
+        }
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int row = 0;
+        int col = cols - 1;
+
+        while (row < rows && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+};
+int main() {
+    Solution sol;
+    vector<vector<int>> matrix = {
+        {1, 4, 7, 11, 15},
+        {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    int target = 5;
+
+    bool found = sol.searchMatrix(matrix, target);
+    if (found) {
+        cout << "Target " << target << " found in the matrix." << endl;
+    } else {
+        cout << "Target " << target << " not found in the matrix." << endl;
+    }
+
+    return 0;
+}
